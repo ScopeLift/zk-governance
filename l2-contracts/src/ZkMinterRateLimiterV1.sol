fe4c4725f9a401f66f318e357606b86882774742
garyghayrat <gary.ghayrat@pm.me>
2025-05-15 11:27:12 -0400

    Enforece rate limit in `mint()`

--- l2-contracts/src/ZkMinterRateLimiterV1.sol
+++ l2-contracts/src/ZkMinterRateLimiterV1.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.24;
 import {IMintable} from "src/interfaces/IMintable.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
-
+import {console2} from "forge-std/console2.sol";
 /// @title ZkMinterRateLimiterV1
 /// @author [ScopeLift](https://scopelift.co)
 /// @notice A contract that implements rate limiting for token minting, allowing authorized minters to collectively mint
@@ -15,16 +15,28 @@ contract ZkMinterRateLimiterV1 is IMintable, AccessControl, Pausable {
   /// @notice The contract where the tokens will be minted by an authorized minter.
   IMintable public mintable;
 
+  /// @notice The number of tokens minted in each mint window.
+  mapping(uint48 mintWindowStart => uint256 mintedAmount) public mintedInWindow;
+
   /// @notice The maximum number of tokens that may be minted by the minter in a single mint rate limit window.
   uint256 public mintRateLimit;
 
   /// @notice The number of seconds in a mint rate limit window.
   uint48 public mintRateLimitWindow;
 
+  /// @notice The timestamp when minting can begin.
+  uint48 public immutable START_TIME;
+
   /// @notice The unique identifier constant used to represent the minter role. An address that has this role may call
   /// the `mint` method, creating new tokens and assigning them to specified address. This role may be granted or
   /// revoked by the DEFAULT_ADMIN_ROLE.
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+  /// @notice Emitted when tokens are minted.
+  event Minted(address indexed minter, address indexed to, uint256 amount);
+
+  /// @notice Error for when the rate limit per mint window is exceeded.
+  error ZkMinterRateLimiterV1__MintRateLimitExceeded(address minter, uint256 amount);
 
   /// @notice Initializes the rate limiter with the mintable contract, admin, mint rate limit, and mint rate limit
   /// window.
@@ -37,6 +49,7 @@ contract ZkMinterRateLimiterV1 is IMintable, AccessControl, Pausable {
     mintable = _mintable;
     mintRateLimit = _mintRateLimit;
     mintRateLimitWindow = _mintRateLimitWindow;
+    START_TIME = uint48(block.timestamp);
 
     _grantRole(DEFAULT_ADMIN_ROLE, _admin);
   }
@@ -46,6 +59,33 @@ contract ZkMinterRateLimiterV1 is IMintable, AccessControl, Pausable {
   /// @param _amount The quantity of tokens that will be minted.
   function mint(address _to, uint256 _amount) external {
     _checkRole(MINTER_ROLE, msg.sender);
+    uint48 _currentMintWindowStart = currentMintWindowStart();
+    _revertIfRateLimitPerMintWindowExceeded(_currentMintWindowStart, _amount);
+
+    mintedInWindow[_currentMintWindowStart] += _amount;
     mintable.mint(_to, _amount);
+    emit Minted(msg.sender, _to, _amount);
+  }
+
+  /// @notice Calculates the start timestamp of the current mint window.
+  /// @return The timestamp marking the start of the current mint window.
+  function currentMintWindowStart() public view returns (uint48) {
+    return uint48(block.timestamp - (block.timestamp - START_TIME) % mintRateLimitWindow);
+  }
+
+  /// @notice Calculates how many tokens are still available to mint in a given window.
+  /// @param _windowStart The timestamp marking the start of the window.
+  /// @return The number of tokens that can still be minted in the given window.
+  function _amountAvailableForMintInWindow(uint48 _windowStart) internal view returns (uint256) {
+    return mintRateLimit - mintedInWindow[_windowStart];
+  }
+
+  /// @notice Reverts if the rate limit per mint window is exceeded.
+  /// @param _windowStart The timestamp marking the start of the window.
+  /// @param _amount The amount of tokens that will be minted.
+  function _revertIfRateLimitPerMintWindowExceeded(uint48 _windowStart, uint256 _amount) internal view {
+    if (_amount > _amountAvailableForMintInWindow(_windowStart)) {
+      revert ZkMinterRateLimiterV1__MintRateLimitExceeded(msg.sender, _amount);
+    }
   }
 }
