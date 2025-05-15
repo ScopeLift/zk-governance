1590b9ac3dcc0314e18cddf365bb8e0d83b27845
garyghayrat <gary.ghayrat@pm.me>
2025-05-15 11:27:12 -0400

    Use cappedMinter as the mint taget

--- l2-contracts/src/ZkMinterRateLimiterV1.sol
+++ l2-contracts/src/ZkMinterRateLimiterV1.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.24;
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
 import {IMintable} from "src/interfaces/IMintable.sol";
+import {ZkCappedMinterV2} from "src/ZkCappedMinterV2.sol";
 
 /// @title ZkMinterRateLimiterV1
 /// @author [ScopeLift](https://scopelift.co)
@@ -12,7 +13,7 @@ import {IMintable} from "src/interfaces/IMintable.sol";
 /// @custom:security-contact security@matterlabs.dev
 contract ZkMinterRateLimiterV1 is IMintable, AccessControl, Pausable {
   /// @notice The contract where the tokens will be minted by an authorized minter.
-  IMintable public mintable;
+  ZkCappedMinterV2 public zkCappedMinter;
 
   /// @notice The maximum number of tokens that may be minted by the minter in a single mint period.
   uint256 public capPerMintPeriod;
@@ -20,13 +21,15 @@ contract ZkMinterRateLimiterV1 is IMintable, AccessControl, Pausable {
   /// @notice The number of seconds in a mint period.
   uint48 public mintPeriod;
 
-  constructor(IMintable _mintable, address _admin, uint256 _capPerMintPeriod, uint48 _mintPeriod) {
-    mintable = _mintable;
+  constructor(ZkCappedMinterV2 _zkCappedMinter, address _admin, uint256 _capPerMintPeriod, uint48 _mintPeriod) {
+    zkCappedMinter = _zkCappedMinter;
     capPerMintPeriod = _capPerMintPeriod;
     mintPeriod = _mintPeriod;
 
     _grantRole(DEFAULT_ADMIN_ROLE, _admin);
   }
 
-  function mint(address _to, uint256 _amount) external virtual {}
+  function mint(address _to, uint256 _amount) external virtual {
+    zkCappedMinter.mint(_to, _amount);
+  }
 }
--- l2-contracts/test/ZkMinterRateLimiterV1.t.sol
+++ l2-contracts/test/ZkMinterRateLimiterV1.t.sol
@@ -1,24 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import {ZkTokenTest} from "test/utils/ZkTokenTest.sol";
-import {IMintable} from "src/interfaces/IMintable.sol";
 import {ZkMinterRateLimiterV1} from "src/ZkMinterRateLimiterV1.sol";
+import {ZkCappedMinterV2, ZkCappedMinterV2Test} from "test/ZkCappedMinterV2.t.sol";
 
-contract ZkMinterRateLimiterV1Test is ZkTokenTest {
+contract ZkMinterRateLimiterV1Test is ZkCappedMinterV2Test {
   ZkMinterRateLimiterV1 public minterRateLimiter;
-  IMintable public mintable = IMintable(makeAddr("mintable"));
   uint256 public constant CAP_PER_MINT_PERIOD = 100_000e18;
   uint48 public constant MINT_PERIOD = 1 days;
 
   function setUp() public override {
     super.setUp();
-    minterRateLimiter = new ZkMinterRateLimiterV1(mintable, admin, CAP_PER_MINT_PERIOD, MINT_PERIOD);
+    minterRateLimiter = new ZkMinterRateLimiterV1(cappedMinter, admin, CAP_PER_MINT_PERIOD, MINT_PERIOD);
   }
 
   function test_InitializesMinterRateLimiterCorrectly() public {
     assertTrue(minterRateLimiter.hasRole(minterRateLimiter.DEFAULT_ADMIN_ROLE(), admin));
-    assertEq(address(minterRateLimiter.mintable()), address(mintable));
+    assertEq(address(minterRateLimiter.zkCappedMinter()), address(cappedMinter));
     assertEq(minterRateLimiter.capPerMintPeriod(), CAP_PER_MINT_PERIOD);
     assertEq(minterRateLimiter.mintPeriod(), MINT_PERIOD);
   }
@@ -26,15 +24,15 @@ contract ZkMinterRateLimiterV1Test is ZkTokenTest {
 
 contract Constructor is ZkMinterRateLimiterV1Test {
   function testFuzz_InitializesMinterRateLimiterCorrectly(
-    IMintable _mintable,
+    ZkCappedMinterV2 _zkCappedMinter,
     address _admin,
     uint256 _capPerMintPeriod,
     uint48 _mintPeriod
   ) public {
     ZkMinterRateLimiterV1 _minterRateLimiter =
-      new ZkMinterRateLimiterV1(_mintable, _admin, _capPerMintPeriod, _mintPeriod);
+      new ZkMinterRateLimiterV1(_zkCappedMinter, _admin, _capPerMintPeriod, _mintPeriod);
 
-    assertEq(address(_minterRateLimiter.mintable()), address(_mintable));
+    assertEq(address(_minterRateLimiter.zkCappedMinter()), address(_zkCappedMinter));
     assertTrue(_minterRateLimiter.hasRole(_minterRateLimiter.DEFAULT_ADMIN_ROLE(), _admin));
     assertEq(_minterRateLimiter.capPerMintPeriod(), _capPerMintPeriod);
     assertEq(_minterRateLimiter.mintPeriod(), _mintPeriod);
