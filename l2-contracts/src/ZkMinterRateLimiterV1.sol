28d97b4e3445964fbd12ad75fd07dc1f8574052b
Gary Ghayrat <61768337+garyghayrat@users.noreply.github.com>
2025-05-21 09:39:58 -0400

    Create initial rate limiter contract (#45)

---
+++ l2-contracts/src/ZkMinterRateLimiterV1.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {IMintable} from "src/interfaces/IMintable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
+
+/// @title ZkMinterRateLimiterV1
+/// @author [ScopeLift](https://scopelift.co)
+/// @notice A contract that implements rate limiting for token minting, allowing authorized minters to collectively mint
+/// up to a specified amount within a configurable time period.
+/// @custom:security-contact security@matterlabs.dev
+contract ZkMinterRateLimiterV1 is IMintable, AccessControl, Pausable {
+  /// @notice The contract where the tokens will be minted by an authorized minter.
+  IMintable public mintable;
+
+  /// @notice The maximum number of tokens that may be minted by the minter in a single mint rate limit window.
+  uint256 public mintRateLimit;
+
+  /// @notice The number of seconds in a mint rate limit window.
+  uint48 public mintRateLimitWindow;
+
+  /// @notice Initializes the rate limiter with the mintable contract, admin, mint rate limit, and mint rate limit
+  /// window.
+  /// @param _mintable A contract used as a target when calling mint. Any contract that conforms to the IMintable
+  /// interface can be used, but in most cases this will be another `ZKMinter` extension or `ZKCappedMinter`.
+  /// @param _admin The address that will have admin privileges.
+  /// @param _mintRateLimit The maximum number of tokens that can be minted during the rate limit window.
+  /// @param _mintRateLimitWindow The duration of the rate limit window in seconds.
+  constructor(IMintable _mintable, address _admin, uint256 _mintRateLimit, uint48 _mintRateLimitWindow) {
+    mintable = _mintable;
+    mintRateLimit = _mintRateLimit;
+    mintRateLimitWindow = _mintRateLimitWindow;
+
+    _grantRole(DEFAULT_ADMIN_ROLE, _admin);
+  }
+
+  /// @notice Mints a given amount of tokens to a given address, so long as the rate limit is not exceeded.
+  /// @param _to The address that will receive the new tokens.
+  /// @param _amount The quantity of tokens that will be minted.
+  function mint(address _to, uint256 _amount) external virtual {
+    mintable.mint(_to, _amount);
+  }
+}
---
+++ l2-contracts/test/ZkMinterRateLimiterV1.t.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {ZkMinterRateLimiterV1} from "src/ZkMinterRateLimiterV1.sol";
+import {ZkCappedMinterV2Test} from "test/ZkCappedMinterV2.t.sol";
+import {IMintable} from "src/interfaces/IMintable.sol";
+
+contract ZkMinterRateLimiterV1Test is ZkCappedMinterV2Test {
+  ZkMinterRateLimiterV1 public minterRateLimiter;
+  IMintable public mintable = IMintable(address(cappedMinter));
+  uint256 public constant MINT_RATE_LIMIT = 100_000e18;
+  uint48 public constant MINT_RATE_LIMIT_WINDOW = 1 days;
+
+  function setUp() public override {
+    super.setUp();
+    minterRateLimiter = new ZkMinterRateLimiterV1(mintable, admin, MINT_RATE_LIMIT, MINT_RATE_LIMIT_WINDOW);
+  }
+
+  function test_InitializesMinterRateLimiterCorrectly() public {
+    assertTrue(minterRateLimiter.hasRole(minterRateLimiter.DEFAULT_ADMIN_ROLE(), admin));
+    assertEq(address(minterRateLimiter.mintable()), address(mintable));
+    assertEq(minterRateLimiter.mintRateLimit(), MINT_RATE_LIMIT);
+    assertEq(minterRateLimiter.mintRateLimitWindow(), MINT_RATE_LIMIT_WINDOW);
+  }
+}
+
+contract Constructor is ZkMinterRateLimiterV1Test {
+  function testFuzz_InitializesMinterRateLimiterCorrectly(
+    IMintable _mintable,
+    address _admin,
+    uint256 _mintRateLimit,
+    uint48 _mintRateLimitWindow
+  ) public {
+    ZkMinterRateLimiterV1 _minterRateLimiter =
+      new ZkMinterRateLimiterV1(_mintable, _admin, _mintRateLimit, _mintRateLimitWindow);
+
+    assertEq(address(_minterRateLimiter.mintable()), address(_mintable));
+    assertTrue(_minterRateLimiter.hasRole(_minterRateLimiter.DEFAULT_ADMIN_ROLE(), _admin));
+    assertEq(_minterRateLimiter.mintRateLimit(), _mintRateLimit);
+    assertEq(_minterRateLimiter.mintRateLimitWindow(), _mintRateLimitWindow);
+  }
+}
